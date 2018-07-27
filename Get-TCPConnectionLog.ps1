999ffac3513dc6216d7bd88681ea103b01522473
scucca <emil@8bit.is>
2018-07-27 19:06:13 +0000

    Added Get-TCPConnectionLog.ps1

---
+++ Get-TCPConnectionLog.ps1
@@ -0,0 +1,67 @@
+function Get-TCPConnectionLog {
+    <#
+    .SYNOPSIS
+        Collect TCP Connections and optionally log to file
+        
+    .NOTES
+        Author: Emil Holm Halldórsson (ehalldorsson@kpmg.is)       
+        Version History:
+        Version 1.0 - Date 27.07.2018
+        Initial Creation
+    #>
+
+    #requires -Version 2
+
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    Param(
+        [Parameter(Mandatory = $false)][string]$LogPath,
+        [Parameter(Mandatory = $false)][int]$Frequency = 5,
+        [Parameter(Mandatory = $false)][int]$Seconds = 30
+    )
+    Process {
+        while ($true) {
+            #Filter Listening ports
+            $filter = {$_.Localaddress -ne '::' `
+                    -and $_.Localaddress -ne '0.0.0.0' `
+                    -and $_.Localaddress -ne '127.0.0.1' `
+            }
+            #Collect all current TCP Connections according to filter
+            $currentConnections = get-nettcpconnection | Where-Object $filter
+
+            if ($lastoutput -ne $null) {# if first time running
+
+                $newConnections = Compare-Object $lastoutput $currentConnections -passthru | Where-Object {$_.Sideindicator -eq '=>'}
+
+            }
+            else {
+                $newConnections = $currentConnections
+            }
+            
+            $lastoutput = $currentConnections
+
+            foreach ($connection in $newConnections) {
+                $output = [pscustomobject] @{
+                    CreationTime  = $connection.CreationTime
+                    LocalAddress  = $connection.Localaddress
+                    LocalPort     = $connection.LocalPort
+                    RemoteAddress = $connection.RemoteAddress
+                    RemotePort    = $connection.RemotePort
+                    State         = $connection.State
+                    ProcessID     = $connection.OwningProcess
+                    ProcessName   = get-process -Id $connection.OwningProcess | select-object -expandproperty processname
+                }
+                if ($Logpath -ne '') {
+                    
+                    $output | Export-CSV -Append -Encoding UTF8 -NoTypeInformation -Path $LogPath
+                }
+                else {
+                    Write-Output $output
+                }
+            }
+
+            start-sleep -seconds 5
+        }
+
+    }
+}
+Get-TCPConnectionLog C:\temp\Logs\$env:Computername-TCPConnectionLog.log
